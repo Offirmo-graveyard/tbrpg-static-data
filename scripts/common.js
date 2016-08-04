44520bca76bf2e8df9e216a364ea794a7d5438dd
Offirmo <offirmo.net@gmail.com>
2016-08-04 06:17:41 +0000

    +

---
+++ scripts/common.js
@@ -0,0 +1,3 @@
+/**
+ * Created by John on 03/08/2016.
+ */
---
+++ scripts/sync.js
@@ -0,0 +1,18 @@
+
+
+const visual_tasks = require('@offirmo/cli-toolbox/stdout/visual_tasks')
+
+visual_tasks.run([
+	{
+		title: 'Gather list of models',
+		task: () => {
+
+		}
+	},
+	{
+		title: 'Syncronize models',
+		task: () => {
+
+		}
+	}
+])
