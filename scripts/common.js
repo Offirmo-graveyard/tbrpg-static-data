8e01a8fc838505b1937b709dd51c9ddbf87ee774
Offirmo <offirmo.net@gmail.com>
2016-08-07 21:26:33 +0000

    +

--- scripts/common.js
+++ scripts/common.js
@@ -2,7 +2,7 @@
 const KEY_SEPARATOR = '|'
 
 function make_primary_key_builder(model_name, schema) {
-	const primary_key_components = schema.offirmo_non_standard.primary_key_components
+	const primary_key_components = schema.offirmo_extensions.primary_key_components
 
 	return data => [
 		model_name,
@@ -14,12 +14,12 @@ function make_i18n_keys_builder(model_name, schema, lang) {
 	const primary_key_builder = make_primary_key_builder(model_name, schema)
 
 	const mandatory_i18n_keys =
-		(schema.offirmo_non_standard.i18n_keys_mandatory['*'] || [])
-		.concat((schema.offirmo_non_standard.i18n_keys_mandatory[lang]|| []))
+		(schema.offirmo_extensions.i18n_keys_mandatory['*'] || [])
+		.concat((schema.offirmo_extensions.i18n_keys_mandatory[lang]|| []))
 
 	const optional_i18n_keys =
-		(schema.offirmo_non_standard.i18n_keys_optional['*'] || [])
-			.concat((schema.offirmo_non_standard.i18n_keys_optional[lang]|| []))
+		(schema.offirmo_extensions.i18n_keys_optional['*'] || [])
+			.concat((schema.offirmo_extensions.i18n_keys_optional[lang]|| []))
 
 	return data => ({
 		mandatory: mandatory_i18n_keys.map(key => primary_key_builder(data) + KEY_SEPARATOR + key),
